bb48633ffe4583de109d80bfc2c4428119d67ae7
QPThree <youngqp3@gmail.com>
2021-06-02 19:59:35 -0700

    added a get all views function and manipulated data in renderchart function to also show views

--- js/app.js
+++ js/app.js
@@ -88,6 +88,16 @@ function getItemClicks(){
   return clicksArr;
 }
 
+//get all item views
+function getItemViews(){
+  let viewsArr = [];
+  for (let i = 0; i < allItems.length; i++){
+    viewsArr.push(allItems[i].viewed);
+  }
+  console.log(viewsArr);
+  return viewsArr;
+}
+
 //use getrandomindex
 //pull object from allItems with index
 //add object.src to img element in index.html
@@ -126,6 +136,26 @@ function renderChart(){
       datasets: [{
         label: '# of Clicks',
         data: getItemClicks(),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      },{
+        label: '# of Views',
+        data: getItemViews(),
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(54, 162, 235, 0.2)',
